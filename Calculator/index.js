7748b30dafe11e23a15013df2a997e31832deabb
hasana9440 <84851251+hasana9440@users.noreply.github.com>
2024-04-24 18:48:38 +0530

    Add files via upload

---
+++ Calculator/index.js
@@ -0,0 +1,19 @@
+let disp = document.getElementById("ip");
+
+function cal(){
+        try{
+            disp.value = eval(disp.value);
+        }
+        catch(error){
+            disp.value = 'ERROR';
+        }
+}
+
+function display(ip){
+    disp.value +=ip;
+}
+
+
+function clearscreen(){
+    disp.value = ""
+}
