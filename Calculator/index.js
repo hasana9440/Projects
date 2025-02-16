1d0cdd63d660c4570ed48e6215ca38d31bdbb1b9
hasana9440 <84851251+hasana9440@users.noreply.github.com>
2025-02-16 18:01:03 +0530

    index.js

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,19 +1,17 @@
 let disp = document.getElementById("ip");
 
-function cal(){
-        try{
-            disp.value = eval(disp.value);
-        }
-        catch(error){
-            disp.value = 'ERROR';
-        }
+function cal() {
+    try {
+        disp.value = eval(disp.value);
+    } catch (error) {
+        disp.value = 'ERROR';
+    }
 }
 
-function display(ip){
-    disp.value +=ip;
+function display(ip) {
+    disp.value += ip;
 }
 
-
-function clearscreen(){
-    disp.value = ""
+function clearscreen() {
+    disp.value = "";
 }
